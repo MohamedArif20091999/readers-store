901186d43572ea047ff5f506bb61e659a963bb39
MohamedArif20091999 <mohamedarifcbe@gmail.com>
2021-06-28 16:25:00 +0530

    feat: passport auth fixed with sequelize

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require('express');
 const passport = require('passport');
 const keys = require('./config/keys');
 const authRoutes = require('./routes/authRoutes');
-const pool = require('./pool');
+const sequelize = require('./db');
+const User = require('./models/user');
+const cookieSession = require('cookie-session');
 require('./services/passport');
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.send({ hello: 'world' });
@@ -13,6 +25,12 @@ app.get('/', (req, res) => {
 
 app.use('/auth', authRoutes);
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log('Server startted');
-});
+try {
+  sequelize.sync().then(
+    app.listen(process.env.PORT || 8080, () => {
+      console.log('Server started');
+    })
+  );
+} catch (err) {
+  console.log(err);
+}
