84154df7eeb0b13ac19e51dc0398c24b857403cd
MohamedArif20091999 <mohamedarifcbe@gmail.com>
2021-06-29 10:03:46 +0530

    feat: added all products route

--- controllers/product.js
+++ controllers/product.js
@@ -16,3 +16,8 @@ exports.addProduct = async (req, res) => {
   console.log(book);
   res.status(201).send(book);
 };
+
+exports.getProducts = async (req, res) => {
+  const books = await Book.findAll();
+  res.status(200).send(books);
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/product');
 
+router.get('/all-products', productsController.getProducts);
 router.post('/add-product', productsController.addProduct);
 
 module.exports = router;
