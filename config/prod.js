5979274fec0aa58d9862ee95af6d39cc60d83b8c
MohamedArif20091999 <mohamedarifcbe@gmail.com>
2021-06-29 21:18:05 +0530

    feat: client and server connection with prod conf

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  pgConString: process.env.PG_CON_STRING,
+  cookieKey: process.env.COOKIE_KEY,
+};
