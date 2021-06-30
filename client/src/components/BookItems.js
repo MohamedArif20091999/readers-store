407501874d8c476fbe39e4d561d8c35bc3eb33f6
MohamedArif20091999 <mohamedarifcbe@gmail.com>
2021-06-30 22:52:01 +0530

    feat: render prods in UI

--- client/src/components/BookItems.js
+++ client/src/components/BookItems.js
@@ -8,7 +8,6 @@ import './css/Card.css';
 const BookItems = () => {
   const dispatch = useDispatch();
   let products = useSelector((state) => state.products);
-
   useEffect(() => {
     dispatch(fetchProducts());
   }, []);
@@ -17,318 +16,81 @@ const BookItems = () => {
   const imageURL =
     'https://images.unsplash.com/photo-1624542313043-40df8401b342?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80';
 
+  const renderItems = () => {
+    if (products.length) {
+      return (
+        <React.Fragment>
+          {products.map((product) => (
+            <Card className="item">
+              <CardActionArea>
+                <CardMedia className="card-image" image={imageURL} title="Contemplative Reptile" />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {product.title}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Book description
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <Grid container item>
+                <Box style={{ textAlign: 'center' }}>
+                  <Rating name="half-rating" value={4} readOnly></Rating>
+                </Box>
+                <Box mt={0.5} pl={1}>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    13,453
+                  </Typography>
+                </Box>
+              </Grid>
+              <CardActions className="add-to-cart-btn">
+                <Button className="a-cart-btn" variant="contained" disableElevation>
+                  Add to cart
+                </Button>
+                {/* <Button size="small" color="primary">
+                   Learn More
+                 </Button> */}
+              </CardActions>
+            </Card>
+          ))}
+        </React.Fragment>
+      );
+    }
+    return <h1>Show loader</h1>;
+  };
   return (
-    <div className="BookItems">
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6} md={4} mx="auto">
-          <Card className="item">
-            <CardActionArea>
-              <CardMedia className="card-image" image={imageURL} title="Contemplative Reptile" />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {products[0].title}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  book.description
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <Grid container item>
-              <Box style={{ textAlign: 'center' }}>
-                <Rating name="half-rating" value={4} readOnly></Rating>
-              </Box>
-              <Box mt={0.5} pl={1}>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  13,453
-                </Typography>
-              </Box>
-            </Grid>
-            <CardActions className="add-to-cart-btn">
-              <Button className="a-cart-btn" variant="contained" disableElevation>
-                Add to cart
-              </Button>
-              {/* <Button size="small" color="primary">
-                Learn More
-              </Button> */}
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} mx="auto">
-          <Card className="item">
-            <CardActionArea>
-              <CardMedia className="card-image" image={imageURL} title="Contemplative Reptile" />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Book title
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Book description
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <Grid container item>
-              <Box style={{ textAlign: 'center' }}>
-                <Rating name="half-rating" value={4} readOnly></Rating>
-              </Box>
-              <Box mt={0.5} pl={1}>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  13,453
-                </Typography>
-              </Box>
-            </Grid>
-            <CardActions className="add-to-cart-btn">
-              {/* <Button className="a-cart-btn" variant="contained" disableElevation>
-                Add to cart
-              </Button> */}
-              <Button href="/auth/google" className="a-cart-btn" variant="contained" disableElevation>
-                go to goole
-              </Button>
-              <Button href="/auth/api/user" className="a-cart-btn" variant="contained" disableElevation>
-                show user
-              </Button>
-              <Button href="/product/all-products" className="a-cart-btn" variant="contained" disableElevation>
-                show prods
-              </Button>
-              {/* <Button size="small" color="primary">
-                Learn More
-              </Button> */}
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} mx="auto">
-          <Card className="item">
-            <CardActionArea>
-              <CardMedia className="card-image" image={imageURL} title="Contemplative Reptile" />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Book title
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Book description
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <Grid container item>
-              <Box style={{ textAlign: 'center' }}>
-                <Rating name="half-rating" value={4} readOnly></Rating>
-              </Box>
-              <Box mt={0.5} pl={1}>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  13,453
-                </Typography>
-              </Box>
-            </Grid>
-            <CardActions className="add-to-cart-btn">
-              <Button className="a-cart-btn" variant="contained" disableElevation>
-                Add to cart
-              </Button>
-              {/* <Button size="small" color="primary">
-                Learn More
-              </Button> */}
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} mx="auto">
-          <Card className="item">
-            <CardActionArea>
-              <CardMedia className="card-image" image={imageURL} title="Contemplative Reptile" />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Book title
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Book description
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <Grid container item>
-              <Box style={{ textAlign: 'center' }}>
-                <Rating name="half-rating" value={4} readOnly></Rating>
-              </Box>
-              <Box mt={0.5} pl={1}>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  13,453
-                </Typography>
-              </Box>
-            </Grid>
-            <CardActions className="add-to-cart-btn">
-              <Button className="a-cart-btn" variant="contained" disableElevation>
-                Add to cart
-              </Button>
-              {/* <Button size="small" color="primary">
-                Learn More
-              </Button> */}
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} mx="auto">
-          <Card className="item">
-            <CardActionArea>
-              <CardMedia className="card-image" image={imageURL} title="Contemplative Reptile" />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Book title
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Book description
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <Grid container item>
-              <Box style={{ textAlign: 'center' }}>
-                <Rating name="half-rating" value={4} readOnly></Rating>
-              </Box>
-              <Box mt={0.5} pl={1}>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  13,453
-                </Typography>
-              </Box>
-            </Grid>
-            <CardActions className="add-to-cart-btn">
-              <Button className="a-cart-btn" variant="contained" disableElevation>
-                Add to cart
-              </Button>
-              {/* <Button size="small" color="primary">
-                Learn More
-              </Button> */}
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} mx="auto">
-          <Card className="item">
-            <CardActionArea>
-              <CardMedia className="card-image" image={imageURL} title="Contemplative Reptile" />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Book title
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Book description
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <Grid container item>
-              <Box style={{ textAlign: 'center' }}>
-                <Rating name="half-rating" value={4} readOnly></Rating>
-              </Box>
-              <Box mt={0.5} pl={1}>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  13,453
-                </Typography>
-              </Box>
-            </Grid>
-            <CardActions className="add-to-cart-btn">
-              <Button className="a-cart-btn" variant="contained" disableElevation>
-                Add to cart
-              </Button>
-              {/* <Button size="small" color="primary">
-                Learn More
-              </Button> */}
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} mx="auto">
-          <Card className="item">
-            <CardActionArea>
-              <CardMedia className="card-image" image={imageURL} title="Contemplative Reptile" />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Book title
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Book description
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <Grid container item>
-              <Box style={{ textAlign: 'center' }}>
-                <Rating name="half-rating" value={4} readOnly></Rating>
-              </Box>
-              <Box mt={0.5} pl={1}>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  13,453
-                </Typography>
-              </Box>
-            </Grid>
-            <CardActions className="add-to-cart-btn">
-              <Button className="a-cart-btn" variant="contained" disableElevation>
-                Add to cart
-              </Button>
-              {/* <Button size="small" color="primary">
-                Learn More
-              </Button> */}
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} mx="auto">
-          <Card className="item">
-            <CardActionArea>
-              <CardMedia className="card-image" image={imageURL} title="Contemplative Reptile" />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Book title
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Book description
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <Grid container item>
-              <Box style={{ textAlign: 'center' }}>
-                <Rating name="half-rating" value={4} readOnly></Rating>
-              </Box>
-              <Box mt={0.5} pl={1}>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  13,453
-                </Typography>
-              </Box>
-            </Grid>
-            <CardActions className="add-to-cart-btn">
-              <Button className="a-cart-btn" variant="contained" disableElevation>
-                Add to cart
-              </Button>
-              {/* <Button size="small" color="primary">
-                Learn More
-              </Button> */}
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} mx="auto">
-          <Card className="item">
-            <CardActionArea>
-              <CardMedia className="card-image" image={imageURL} title="Contemplative Reptile" />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Book title
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Book description
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <Grid container item>
-              <Box style={{ textAlign: 'center' }}>
-                <Rating name="half-rating" value={4} readOnly></Rating>
-              </Box>
-              <Box mt={0.5} pl={1}>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  13,453
-                </Typography>
-              </Box>
-            </Grid>
-            <CardActions className="add-to-cart-btn">
-              <Button className="a-cart-btn" variant="contained" disableElevation>
-                Add to cart
-              </Button>
-              {/* <Button size="small" color="primary">
-                Learn More
-              </Button> */}
-            </CardActions>
-          </Card>
-        </Grid>
+    // <div className="BookItems">
+    <Grid container spacing={3}>
+      <Grid item xs={12} sm={6} md={4} mx="auto">
+        {renderItems()}
       </Grid>
-    </div>
+    </Grid>
+    // </div>
   );
 };
 
 export default BookItems;
+
+// const renderItems = () => {
+//   if (products.length) {
+//     return (
+//       <div>
+//         {products.map((product) => (
+
+//         ))}
+//         )
+//       </div>
+//     );
+//   }
+//   return <h1>Show loader</h1>;
+// };
+
+// return (
+//   <div className="BookItems">
+//     {renderItems()}
+//     <Grid container spacing={3}>
+//       <Grid item xs={12} sm={6} md={4} mx="auto"></Grid>
+//       {renderItems()}
+//     </Grid>
+//   </div>
+// );
