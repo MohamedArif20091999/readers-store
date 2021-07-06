efde7254672f8432005d01a1966422ca9efcd7aa
MohamedArif20091999 <mohamedarifcbe@gmail.com>
2021-07-06 22:49:23 +0530

    fix: linted

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,10 +1,6 @@
 import { AppBar } from '@material-ui/core';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import React from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { fetchProducts } from '../actions';
-// import Header from './Appbar';
-// import BookItems from './BookItems';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './Home';
 import CheckOut from './Checkout';
@@ -21,8 +17,6 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <div>
-          {/* <Home></Home> */}
-
           <Route path="/" exact component={Home} />
           <Route path="/checkout" component={CheckOut} />
         </div>
